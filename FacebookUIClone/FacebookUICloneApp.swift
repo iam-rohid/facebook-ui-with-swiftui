a8cd3623437a25d820177a0644432c4f890ba5fc
Rohidul Islam <rohidul209@gmail.com>
2021-12-27 00:38:00 +0600

    Initial Commit

---
+++ FacebookUIClone/FacebookUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookUICloneApp.swift
+//  FacebookUIClone
+//
+//  Created by Rohid on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct FacebookUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
